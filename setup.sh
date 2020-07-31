11887a594016d76ec2ddbe311d077e5789d2ccc5
William McKinnon <services@willmckinnon.com>
2020-07-30 22:43:21 -0400

    generates full config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Check if script is being ran as root
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run as root."
     exit 1
@@ -15,11 +16,29 @@ git clone git://github.com/linux-surface/linux-surface.git
 KERNEL_VER=$(uname -r | cut -d . -f -2)
 PATCHES_SRC="$PWD/linux-surface/patches/$KERNEL_VER"
 PATCHES_DST='/etc/portage/patches/sys-kernel/gentoo-sources'
+CONFIG_SRC="$PWD/linux-surface/configs/surface-$KERNEL_VER.config"
+KERNEL_PATH='/usr/src/linux'
+MAKEFLAGS='-j5'
 
+# Patch kernel if patches exist
 if [ -d "$PATCHES_SRC" ]; then
     mkdir -pv "$PATCHES_DST"
     cp -v "$PATCHES_SRC"/* "$PATCHES_DST"
     emerge -av sys-kernel/gentoo-sources
 else
     echo "Patches for linux kernel $KERNEL_VER not found"
+    exit 1
 fi
+
+# Merge relevant config options if they exist
+if [ -e "$CONFIG_SRC" ]; then
+    echo "Config for kernel $KERNEL_VER found!"
+    echo "Merging Configs..."
+    sh "$KERNEL_PATH/scripts/kconfig/merge_config.sh" "$KERNEL_PATH/.config" "$CONFIG_SRC"
+fi
+
+# Install kernel
+make -C "$KERNEL_PATH" "$MAKEFLAGS"
+make -C "$KERNEL_PATH" modules_install
+make -C "$KERNEL_PATH" install
+emerge -1v @module-rebuild
